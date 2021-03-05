18abba662dc0868c316e84696b6c4cac70379702
3027_ильиных_дв <dimkaspaun@mail.ru>
2021-03-05 14:20:54 +0700

    ""

--- GB_HW_6/HW_6_2.py
+++ GB_HW_6/HW_6_2.py
@@ -13,14 +13,14 @@ class Road:
     _length = None
     _width = None
     __asphalt = 25  # кг
-    __depth = 0.05  # м
+    __depth = 5  # м
 
     def __init__(self, length, width):
         self._width = width
         self._length = length
 
     def calculate(self):
-        return self._length * self._width * self.__asphalt * self.__depth
+        return (self._length * self._width * self.__asphalt * self.__depth) / 1000
 
 
 road = Road(length=5000, width=20)
